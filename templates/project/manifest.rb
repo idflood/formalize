7b4f77cafeda60e7604692716a765ca9a649be08
David Mignot <david.mig@gmail.com>
2011-09-22 15:59:25 +0200

    simple manifest

---
+++ templates/project/manifest.rb
@@ -0,0 +1,8 @@
+description "Formalize compass extension."
+discover :all
+
+welcome_message %Q{
+To complete the installation of formalize you need 
+to import one of the provided javascript in your html:
+<script src="theme/javascript/jquery.formalize.min.js"></script>
+}
